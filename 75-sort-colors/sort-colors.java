c7f02c320409f1286622c132a25820dba73d8691
Rohit Kumar <168295787+rohitkumar087@users.noreply.github.com>
2025-12-20 00:05:16 +0530

    Time: 0 ms (100.00%) | Memory: 43.6 MB (12.63%) - LeetSync

--- 75-sort-colors/sort-colors.java
+++ 75-sort-colors/sort-colors.java
@@ -1,32 +1,51 @@
 class Solution {
     public void sortColors(int[] nums) {
-        int zero = 0;
-        int one = 0;
-        int two = 0;
+    //     int zero = 0;
+    //     int one = 0;
+    //     int two = 0;
 
-        for(int i=0;i<nums.length;i++){
-            if(nums[i]==0){
-                zero++;
+    //     for(int i=0;i<nums.length;i++){
+    //         if(nums[i]==0){
+    //             zero++;
+    //         }
+    //         else if(nums[i]==1){
+    //             one++;
+    //         }
+    //         else if(nums[i]==2){
+    //             two++;
+    //         }
+    //     }
+        
+    //     for(int i=0;i<zero;i++){
+    //         nums[i] = 0;
+    //     }
+    //    for(int j=zero;j<one+zero;j++){
+    //     nums[j]=1;
+    //    }
+    //    for(int j=one+zero;j<one+zero+two;j++){
+    //     nums[j]=2;
+    //    }  
+
+        int i=0;
+        int j=nums.length-1;
+
+        for(int k=0;k<=j;){
+            if(nums[k]==0){
+               int temp = nums[k];
+               nums[k] = nums[i];
+               nums[i]=temp;
+               i++;
+               k++;
             }
-            else if(nums[i]==1){
-                one++;
+            else if(nums[k] == 1){
+                k++;
             }
-            else if(nums[i]==2){
-                two++;
+            else{
+                int temp=nums[k];
+                nums[k]=nums[j];
+                nums[j]=temp;
+                j--;
             }
         }
-        
-        for(int i=0;i<zero;i++){
-            nums[i] = 0;
-        }
-       for(int j=zero;j<one+zero;j++){
-        nums[j]=1;
-       }
-       for(int j=one+zero;j<one+zero+two;j++){
-        nums[j]=2;
-       }
-
-
-        
     }
 }
