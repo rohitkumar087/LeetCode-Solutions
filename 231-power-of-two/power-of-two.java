4ccafde4fda544eddc8266d4e1462ede1b2ca057
Rohit Kumar <168295787+rohitkumar087@users.noreply.github.com>
2026-01-10 15:45:14 +0530

    Time: 1 ms (95.52%) | Memory: 42.6 MB (38.23%) - LeetSync

---
+++ 231-power-of-two/power-of-two.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        while(n>1){
+            if(n%2==1){
+                return false;
+            }
+            n/=2;
+        }
+        if(n == 1){
+            return true;
+        }
+        return false;
+    }
+}
