7aba73a69330b3f52fc7fee76c171b60ae5618ff
Rohit Kumar <168295787+rohitkumar087@users.noreply.github.com>
2026-05-28 16:51:14 +0530

    Time: 2 ms (99.31%) | Memory: 46.2 MB (91.30%) - LeetSync

--- 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
+++ 345-reverse-vowels-of-a-string/reverse-vowels-of-a-string.java
@@ -1,34 +1,30 @@
 class Solution {
     public String reverseVowels(String s) {
-
-        char[] arr = s.toCharArray();   
+        char arr[] = s.toCharArray();
         int start = 0;
-        int end = arr.length - 1;
-
-        while (start < end) {
-
+        int end =  arr.length-1;
+        String ss = "";
+        while(start < end){
             char ch1 = arr[start];
             char ch2 = arr[end];
 
-            boolean v1 = (ch1 == 'a' || ch1 == 'e' || ch1 == 'i' || ch1 == 'o' || ch1 == 'u'
-                       || ch1 == 'A' || ch1 == 'E' || ch1 == 'I' || ch1 == 'O' || ch1 == 'U');
+            boolean first = ch1 == 'a' ||ch1 == 'e' ||ch1 == 'i' ||ch1 == 'o' ||ch1 == 'u' ||ch1 == 'A' ||ch1 == 'E' ||ch1 == 'I' ||ch1 == 'O' ||ch1 == 'U';
 
-            boolean v2 = (ch2 == 'a' || ch2 == 'e' || ch2 == 'i' || ch2 == 'o' || ch2 == 'u'
-                       || ch2 == 'A' || ch2 == 'E' || ch2 == 'I' || ch2 == 'O' || ch2 == 'U');
+            boolean last = ch2 == 'a' ||ch2 == 'e' ||ch2 == 'i' ||ch2 == 'o' ||ch2 == 'u' ||ch2 == 'A' ||ch2 == 'E' ||ch2 == 'I' ||ch2 == 'O' ||ch2 == 'U';
 
-            if (v1 && v2) {
+            if(first && last){
                 char temp = arr[start];
                 arr[start] = arr[end];
                 arr[end] = temp;
-                start++;        
+                start++;
                 end--;
-            } else if (!v1) {
-                start++;        
-            } else {
-                end--;          
             }
+            else if(!first){
+                start++;
+            }
+            else end--;
         }
 
-        return new String(arr);  
+        return new String(arr);
     }
 }
