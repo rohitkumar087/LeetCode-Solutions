d95702343793cc10839ddf5e7c75bad2734735c4
Rohit Kumar <168295787+rohitkumar087@users.noreply.github.com>
2026-06-15 17:54:00 +0530

    Time: 6 ms (99.42%) | Memory: 49.5 MB (83.88%) - LeetSync

--- 49-group-anagrams/group-anagrams.java
+++ 49-group-anagrams/group-anagrams.java
@@ -1,66 +1,16 @@
 class Solution {
-    public List<List<String>> groupAnagrams(String[] strs){
-
-        Map<String,List<String>> ans = new HashMap<>();
-
-        for (int i = 0; i < strs.length; i++) {
-            char[] arr = strs[i].toCharArray();
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String,List<String>> map = new HashMap<>();
+        for(String word : strs){
+            char[] arr = word.toCharArray();
             Arrays.sort(arr);
-            String key = new String(arr);
-            List<String> curr = new ArrayList<>();
-            if(!ans.containsKey(key)){
-                curr.add(strs[i]);
-                ans.put(key , curr);
+            String sorted = new String(arr);
+
+            if(!map.containsKey(sorted)){
+                map.put(sorted,new ArrayList<>());
             }
-            else ans.get(key).add(strs[i]);
-            
+            map.get(sorted).add(word);
         }
-        return new ArrayList<>(ans.values());
-
-
-
-
-
-
-
-
-
-
-
-
-        // public static boolean isAnagram(String s1, String s2) {
-        //    char[] fre = new char[26];
-
-        //    for(char ch : s1.toCharArray()) fre[ch-'a']++;
-        //    for(char ch : s2.toCharArray()) fre[ch-'a']--;
-
-        //    for(char n : fre){
-        //     if(n != 0) return false;
-        //    }
-        //    return true;
-        // }
-        // public List<List<String>> groupAnagrams(String[] strs) {
-        //     List<List<String>> ans = new ArrayList<>();
-        //     Boolean[] Vis = new Boolean[strs.length];
-        //     for(int i=0; i<Vis.length;i++){
-        //         Vis[i]=false;
-        //     }
-
-        //     for(int i=0;i<strs.length;i++){
-        //         if(Vis[i]!=true){
-        //             List<String> curr = new ArrayList<>();
-        //             curr.add(strs[i]);
-        //             Vis[i]=true;
-        //             for(int j=i+1;j<strs.length;j++){
-        //                 if(Vis[j]!=true && isAnagram(strs[i],strs[j])){
-        //                     curr.add(strs[j]);
-        //                     Vis[j] = true;
-        //                 }
-        //             }
-        //             ans.add(curr);
-        //         }
-
-        //     }
-        //     return ans;
+        return new ArrayList<>(map.values());
     }
 }
